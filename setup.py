17cb787fc39181d56c5fcd013573341db6a9c483
Douwe Hoekstra <douwe.hoekstra2512@gmail.com>
2021-08-29 23:45:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labo",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["Click", "Jinja2", "tomli", "rich"],
+    entry_points={
+        "console_scripts": [
+            "labo = labo.main:cli",
+        ],
+    },
+)
