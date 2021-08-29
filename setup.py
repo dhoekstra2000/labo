74d18a06f3a5050a0639947e4d26f6bb76bedcb0
Douwe Hoekstra <douwe.hoekstra2512@gmail.com>
2021-08-30 00:35:04 +0200

    Add LICENSE

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="labo",
     version="0.1.0",
+    license="MIT",
+    author="Douwe Hoekstra",
     packages=find_packages(),
     install_requires=["Click", "Jinja2", "tomli", "rich"],
     entry_points={
