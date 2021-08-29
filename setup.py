91486a26e8dfe4075521fa524ae1cba723c506d4
Douwe Hoekstra <douwe.hoekstra2512@gmail.com>
2021-08-30 01:00:04 +0200

    Convert setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="labo",
-    version="0.1.0",
-    license="MIT",
-    author="Douwe Hoekstra",
-    packages=find_packages(),
-    install_requires=["Click", "Jinja2", "tomli", "rich"],
-    entry_points={
-        "console_scripts": [
-            "labo = labo.main:cli",
-        ],
-    },
-)
+setuptools.setup()
